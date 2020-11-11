4f87856f410bd4b544347812b54eb710a40e8ca4
AMandebura <50381932+AMandebura@users.noreply.github.com>
2020-11-11 11:22:53 +0200

    MECHANIK(+CZLOWIEK)PRAC dom

--- src/Mechanik/mechanik.java
+++ src/Mechanik/mechanik.java
@@ -2,5 +2,14 @@ package Mechanik;
 
 
     public class mechanik extends czlowiek{
-    }
+        protected String drugieimie;
+        boolean czyspuzniasie=true;
+        String wyksztalcenie;
+
+
+        public mechanik(double imie, char plec, String miejsce_zamieszkania, boolean czy_lysy, String color_wlosow, int wiek, czlowiek[] rodzice, String drugieimie , boolean czyspuzniasie, String wyksztalcenie){
+            this.drugieimie = drugieimie;
+            this.czyspuzniasie= czyspuzniasie;
+            this.wyksztalcenie = wyksztalcenie;
+    }}
 
